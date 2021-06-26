9033307380e32034e0b0b74f10deaaf1c684d632
Pablo Martin <pabloenjerez@gmail.com>
2021-06-26 20:19:42 +0200

    Version 1.1 del portfolio

---
+++ profile.js
@@ -0,0 +1,116 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 100,
+  },
+  {
+    skill: "Golang",
+    percentage: 80,
+  },
+  {
+    skill: "Nodejs",
+    percentage: 90,
+  },
+  {
+    skill: "Blockchain",
+    percentage: 40,
+  },
+  {
+    skill: "Physics",
+    percentage: 70,
+  },
+  {
+    skill: "Economics",
+    percentage: 70,
+  },
+  {
+    skill: "Machine Learning",
+    percentage: 70,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software developer at Microsoft",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro ",
+    from: 2000,
+    to: 2005,
+  },
+  {
+    title: "Software developer at Google",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam ",
+    from: 2005,
+    to: 2007,
+  },
+  {
+    title: "Software developer at SpaceX",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem ",
+    from: 2010,
+  },
+];
+
+export const projects = [
+  {
+    name: "Awesome Website 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio1.jpeg",
+  },
+  {
+    name: "Awesome Website 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio2.jpg",
+  },
+  {
+    name: "Awesome Website 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio3.png",
+  },
+  {
+    name: "Awesome Website 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio4.png",
+  },
+  {
+    name: "Awesome Website 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio5.jpeg",
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "portfolio6.jpeg",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://images.ctfassets.net/51xdmtqw3t2p/2w0H06U9MYaJNsonXhyD3I/0cd72a4b4e01460bcd7145e984b05c38/Portada_react.jpg?w=1100&h=800&q=50",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://images.ctfassets.net/51xdmtqw3t2p/2w0H06U9MYaJNsonXhyD3I/0cd72a4b4e01460bcd7145e984b05c38/Portada_react.jpg?w=1100&h=800&q=50",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://images.ctfassets.net/51xdmtqw3t2p/2w0H06U9MYaJNsonXhyD3I/0cd72a4b4e01460bcd7145e984b05c38/Portada_react.jpg?w=1100&h=800&q=50",
+  },
+];
